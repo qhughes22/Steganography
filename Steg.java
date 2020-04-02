109bf8928e0d8a243eee08aae1c5a4ad70147a7d
qhughes22 <49630383+qhughes22@users.noreply.github.com>
2020-04-01 21:28:49 -0400

    made more methods for getting bits

--- Steg.java
+++ Steg.java
@@ -2,13 +2,20 @@
 import java.awt.image.BufferedImage;
 import java.awt.image.WritableRaster;
 import java.io.File;
+import java.lang.reflect.Array;
 import java.util.ArrayList;
 import java.util.Arrays;
 import javax.imageio.ImageIO;
 
 public class Steg {
     public static void main(String[] args) throws Exception {
-        BufferedImage image = ImageIO.read(new File("hide_text.png"));
+
+        System.out.println(binaryToString(getLeastSigBits("hide_text.png")));
+        System.out.println("");
+    }
+
+    public static int[] getLeastSigBits(String filename) throws Exception { //gets least significant bits
+        BufferedImage image = ImageIO.read(new File(filename));
         int width = image.getWidth();
         int height = image.getHeight();
         System.out.println("Height: " + height + " Width: " + width);
@@ -19,18 +26,41 @@ public class Steg {
             for (int c = 0; c < width; c++) {
                 int[] pixels = raster.getPixel(c, r, (int[]) null);
                 if (true) {
-                    all.add((pixels[0] & 1));
-                    all.add((pixels[1] & 1));
-                    all.add((pixels[2] & 1));
+                    all.add(pixels[0] & 1);
+                    all.add(pixels[1] & 1);
+                    all.add(pixels[2] & 1);
                     count++;
                 }
             }
         }
         int[] a = new int[all.size()];
-        for(int i =0;i<all.size();i++)
+        for (int i = 0; i < all.size(); i++)
             a[i] = all.get(i);
-        System.out.println(binaryToString(a));
-        System.out.println("");
+        return a;
+    }
+    public static int[] getSecondLeastSigBits(String filename) throws Exception { //gets least significant bits
+        BufferedImage image = ImageIO.read(new File(filename));
+        int width = image.getWidth();
+        int height = image.getHeight();
+        System.out.println("Height: " + height + " Width: " + width);
+        WritableRaster raster = image.getRaster();
+        int count = 0;
+        ArrayList<Integer> all = new ArrayList<>();
+        for (int r = 0; r < height; r++) {
+            for (int c = 0; c < width; c++) {
+                int[] pixels = raster.getPixel(c, r, (int[]) null);
+                if (true) {
+                    all.add((pixels[0] & 2)>>1);
+                    all.add((pixels[1] & 2)>>1);
+                    all.add((pixels[2] & 2)>>1);
+                    count++;
+                }
+            }
+        }
+        int[] a = new int[all.size()];
+        for (int i = 0; i < all.size(); i++)
+            a[i] = all.get(i);
+        return a;
     }
 
     public static int binaryToUnsignedInt(int[] binary) { //binary array to unsigned int
@@ -42,9 +72,9 @@ public class Steg {
 
     public static String binaryToString(int[] binary) { //binary array to string
         String toReturn = "";
-        int[][] chunks = getChunks(binary,8);
-        for(int[] t: chunks)
-            toReturn+=(char) (binaryArrayToInt(t));
+        int[][] chunks = getChunks(binary, 8);
+        for (int[] t : chunks)
+            toReturn += (char) (binaryArrayToInt(t));
         return toReturn;
     }
 
@@ -54,21 +84,29 @@ public class Steg {
         int numChunks = binary.length / chunksize;
         int[][] toReturn = new int[numChunks][];
         for (int i = 0; i < numChunks; i++) {
-            toReturn[i] = Arrays.copyOfRange(binary, chunksize * i, chunksize *(i+1));
+            toReturn[i] = Arrays.copyOfRange(binary, chunksize * i, chunksize * (i + 1));
         }
         return toReturn;
     }
 
-    public static int arrayToInt(int[] a){ //convert an array of digits into an int
+    public static int arrayToInt(int[] a) { //convert an array of digits into an int
         String s = "";
-        for(int i=0;i<a.length;i++)
+        for (int i = 0; i < a.length; i++)
             s += a[i];
         return Integer.parseInt(s);
     }
-    public static int binaryArrayToInt(int[] a){ //convert a binary array of digits into an int (unsigned I think)
+
+    public static int binaryArrayToInt(int[] a) { //convert a binary array of digits into an int (unsigned I think)
         String s = "";
-        for(int i=0;i<a.length;i++)
+        for (int i = 0; i < a.length; i++)
             s += a[i];
-        return Integer.parseInt(s,2);
+        return Integer.parseInt(s, 2);
+    }
+public static int[] listToArray(ArrayList<Integer> a){
+        int[] q = new int[a.size()];
+        for(int i=0;i<a.size();i++)
+            q[i]=a.get(i);
+        return q;
     }
 }
+
