75d7ed81fe4bc4b82f3203f133db837fac75eee7
rvaninmunoz22 <60942982+rvaninmunoz22@users.noreply.github.com>
2020-04-03 16:11:08 -0500

    Update MagnifyBits.java

--- MagnifyBits.java
+++ MagnifyBits.java
@@ -6,14 +6,17 @@ import javax.imageio.ImageIO;
 public class MagnifyBits {
 
 	public static void main (String[] args) throws Exception {
-		BufferedImage img = magnifyBits(ImageIO.read(new File("hide_image.png")));
+		BufferedImage img = magnifyBitsInImage(ImageIO.read(new File("hide_image.png")));
 		ImageIO.write(img, "png", new File("altered_java.png"));
 		args = new String[1];
 		args[0] = "altered_java.png";
 		HiddenImage.main(args);
 	}
 
-	private static BufferedImage magnifyBits(BufferedImage image) {
+	private static BufferedImage magnifyBitsInImage(BufferedImage image) {
+		// takes an image (such as hide_image.png or hide_text.png) and creates a new image with all pixel channels' LSB magnified
+		// must be called from MagnifyBits.main
+		
 		int width = image.getWidth();
         int height = image.getHeight();
         WritableRaster raster = image.getRaster();
@@ -29,8 +32,16 @@ public class MagnifyBits {
         }
         return image;
 	}
+	
+	public static int[] magnifyBitsInArray(int[] bitstring) {
+		// takes in an int[] passed through a method such as Steg.getLeastSignificantBit and magnifies each bit
+		// can be called from other classes
+		
+		for (int i=0; i<bitstring.length;i++) bitstring[i] = magnify(bitstring[i]);
+		return bitstring;
+	}
 
-	private static int magnify(int p) {
+	public static int magnify(int p) {
 		if ((p & 1) == 1) return 255;
 		else return 0;
 		
